f6ea67f8160680881e0ed746d86f026356599b71
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-12-02 09:17:30 +0100

    inseriti i dottori e testata l'applicazione

--- src/main/java/it/prova/dottori/DottoriApplication.java
+++ src/main/java/it/prova/dottori/DottoriApplication.java
@@ -1,13 +1,37 @@
 package it.prova.dottori;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.prova.dottori.model.Dottore;
+import it.prova.dottori.service.DottoreService;
+
 @SpringBootApplication
-public class DottoriApplication {
+public class DottoriApplication implements CommandLineRunner{
+	
+	@Autowired
+	DottoreService dottoreService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DottoriApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		dottoreService.inserisciNuovo(Dottore.builder()
+				.nome("Mariano")
+				.cognome("miconos")
+				.codiceDottore("THSNBDYWBSU")
+				.build());
+		
+		dottoreService.inserisciNuovo(Dottore.builder()
+				.nome("Ross")
+				.cognome("Bolt")
+				.codiceDottore("ASFGDRDFDGS")
+				.build());
 	}
 
 }
